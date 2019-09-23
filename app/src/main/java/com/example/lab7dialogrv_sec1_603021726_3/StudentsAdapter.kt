7ef6f654a639bbc5b5e9fb7fa52cb24a6db7371a
Nisacholll <53934861+Nisacholll@users.noreply.github.com>
2019-09-23 17:50:51 +0700

    LAB7_SEC1_603021726-3

---
+++ app/src/main/java/com/example/lab7dialogrv_sec1_603021726_3/StudentsAdapter.kt
@@ -0,0 +1,33 @@
+package layout
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab7dialogrv_sec1_603021726_3.R
+import kotlinx.android.synthetic.main.std_item_layout.view.*
+
+class StudentsAdapter (val items : List<Student>, val context : Context) : RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view_item = LayoutInflater.from(parent.context).inflate(R.layout.std_item_layout, parent, false)
+        return ViewHolder(view_item)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvID?.text = "ID : " + items[position].id
+        holder.tvName?.text = "Name : " + items[position].name
+        holder.tvAge?.text = "Age : " + items[position].age.toString()
+    }
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    val tvID = view.tv_id
+    val tvName  = view.tv_name
+    val tvAge = view.tv_age
+}
---
+++ app/src/test/java/com/example/lab7dialogrv_sec1_603021726_3/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.lab7dialogrv_sec1_603021726_3
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
